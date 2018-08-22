d67fe4500adf9e1afe1d07622d20aba03e893c98
EkaterinaSukhanova <katrinsuh@yandex.ru>
2018-08-22 18:01:32 +0300

    not ideally yet

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $("menu li").on("click", function () {
+        if (!$(this).hasClass("active")) {
+            let i = $(this).index();
+            $(".menu li.active").removeClass("active");
+            $(".all-tabs .active").hide().removeClass("active");
+            $(this).addClass("active");
+            $($(".all-tabs").children(".tab")[i]).fadeIn(1000).addClass("active");
+        }
+    });
+
+});
